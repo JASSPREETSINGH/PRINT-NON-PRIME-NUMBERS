8d83b99643f2d5f4dbb09c6c5ae11ee170bd1468
Dell <Dell@192.168.2.16>
2017-10-09 23:15:07 -0400

    printing non prime no with logical errors

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -1,4 +1,4 @@
-/*
+/*  
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -15,7 +15,22 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     
+     int upperbound=100;
+        for (int num = 2; num < upperbound; num++) {
+            {
+                int maxfac=(int) Math.sqrt(num);
+                
+                for (int j = 2; j < maxfac; j++) {
+                    if(num%j==0)
+                    {
+                        System.out.println(num+"is not prime");
+                    }
+                }
+                
+            }
+        }
+// TODO code application logic here
     }
     
 }
